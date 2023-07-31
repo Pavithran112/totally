0b3462ba530e4973461d9aacf2fff9d2830ddf37
Pavithran112 <136552314+Pavithran112@users.noreply.github.com>
2023-07-31 21:55:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// function myfunction(){
+//     let joker = document.getElementById("image1")
+//     if(joker.src.match('Leo_(2023_Indian_film).jpg')){
+//         joker.src='Captain Miller first look.jpg'
+//     }
+//     else{
+//         joker.src= 'Captain Miller first look.jpg'
+//     }
+
+// }
+
+    function changeimage0(){
+        let img2 = document.getElementById("image")
+        img2.src = "OIP (1).jpg" ;
+    }
+
+    function changeimage(){
+            let img = document.getElementById("image")   
+            img.src = "tereiskmein_d.jpg";
+
+            // let img1 = document.getElementById("image1")
+            // img1.src = "fast-x-7680 x 4320-13683.jpg";
+        }
+        
+        function changeimage1(){
+             let img2 = document.getElementById("image")
+            img2.src = "Maamannan.jpg";
+        }
+
+         function changeimage2(){
+             let img2 = document.getElementById("image")
+            img2.src = "spiderman-into-the-spider-verse-characters-uhdpaper.com-8K-15.jpg";
+        }
+
+        function changeimage3(){
+             let img2 = document.getElementById("image")
+            img2.src = "HD-wallpaper-avatar-2-the-way-of-water-banner.jpg" ;
+        }
+
+        function changeimage4(){
+            let img2 = document.getElementById("image")
+           img2.src = "Jawan.webp" ;
+       }
+
